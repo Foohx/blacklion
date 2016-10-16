b31c733a7526b1e998f437f3bd53ed5a128e112b
Foohx <robin.guinant@gmail.com>
2016-10-16 12:23:27 +0200

    3GM  class updated
    - Login added
    - Login check added

--- blacklion.py
+++ blacklion.py
@@ -1,4 +1,11 @@
 # coding: utf-8
+import sys
+from thirdworldwar import *
 
-if __name__ == "__main__":
-    print("Started !")
+if __name__ != "__main__":
+    sys.exit(1)
+
+h = ThirdWorldWar('aezo', '26041994', 3)
+l = h.rLogin()
+
+print(l)
---
+++ thirdworldwar.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+import requests
+from lxml import html
+
+class ThirdWorldWar:
+
+    def __init__(self, username, password, server):
+        self.account = {
+            'email': username,
+            'pass': password,
+            'serveur': int(server),
+            'connexion': ''
+        }
+        self.s = requests.session()
+
+    def rLogin(self):
+        r = self.s.post('http://www.3gm.fr/index.php', data=self.account)
+        r = self.s.get('http://www.3gm.fr/game/index.php')
+        return self.isLogged()
+
+    def isLogged(self):
+        r = self.s.get('http://www.3gm.fr/game/index.php')
+        tree = html.fromstring(r.content)
+        nav = tree.xpath('//div[@id="gauche"]/div[@class="menu"]/a/@href')
+        if len(nav) == 0 or '../index.php?action=deco' not in nav:
+            return False
+        return True
