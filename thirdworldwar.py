4e77d46cda6ba6d086348c5d1af35728c8cbf17b
Foohx <robin.guinant@gmail.com>
2016-10-16 14:11:14 +0200

    3GM class updated again
    Class is now able to send a message to :
    - Serveur (Global Tchat)
    - Alliance tchat
    - A specific user
    Token getter is also a bit best

--- thirdworldwar.py
+++ thirdworldwar.py
@@ -23,6 +23,45 @@ class ThirdWorldWar:
         r = self.s.get('http://www.3gm.fr/index.php?action=deco')
         return True if not self.isLogged() else False
 
+    def rThis(self, url):
+        r = self.s.get(url)
+        return r
+
+    def rSendMessageToServer(self, message, token=False):
+        r = self.s.get('http://www.3gm.fr/game/tchat.php')
+        if token == False:
+            b, token = self.getToken(r.content)
+            if not b:
+                return False
+        r = self.s.post('http://www.3gm.fr/game/tchat.php', data={
+            'pseudo': self.account['email'],
+            'tk': token,
+            'message': message,
+        })
+        return True
+
+    def rSendMessageToAlliance(self, message):
+        r = self.s.get('http://www.3gm.fr/game/my_ally.php')
+        b, token = self.getToken(r.content)
+        print(token)
+        if not b:
+            return False
+        r = self.s.post('http://www.3gm.fr/game/my_ally.php', data={
+            'pseudo': self.account['email'],
+            'tk': token,
+            'message': message,
+        })
+        return True
+
+    def rSendMessageToUser(self, message, user):
+        r = self.s.post('http://www.3gm.fr/game/modules/ajax/ajax_messagerie.php', data={
+            'correspondant_post_new': user,
+            'message_post_new': message,
+            'id_destinataire': "",
+        })
+        # print(r.content)
+        return True
+
     def isLogged(self):
         r = self.s.get('http://www.3gm.fr/game/index.php')
         tree = html.fromstring(r.content)
@@ -35,7 +74,19 @@ class ThirdWorldWar:
         if not html:
             r = self.s.get('http://www.3gm.fr/game/index.php')
             html = r.content
-        m = re.search('&tk=([a-z0-9]+)', html)
-        if m == None:
-            return False, ""
-        return True, m.group(1)
+        expr = [
+            '&tk=([a-z0-9]+)',
+            'tk=([a-z0-9]+)',
+            "<input type=hidden value='([a-z0-9]+)' name='tk' \/>",
+            "<input type='hidden' name='tk' value='([a-z0-9]+)'/>",
+            "<input type=\"hidden\" name=\"tk\" value='([a-z0-9]+)' \/>"
+        ]
+        token = None
+        for e in expr:
+            m = re.search(e, html)
+            if m != None:
+                token = m.group(1)
+                break
+        if token == None:
+            return False, None
+        return True, token
