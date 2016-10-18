0576363efc993cb717148542768a3813e246f391
Foohx <robin.guinant@gmail.com>
2016-10-18 15:18:25 +0200

    Bot can now start build buildings

--- blacklion.py
+++ blacklion.py
@@ -47,9 +47,23 @@ def selectStep(datas):
         i = i +1
     return step
 
+def getIndexFor(search, into):
+    count = 0
+    for i in into:
+        if i['name'] == search:
+            break
+        count = count +1
+    return count
+
+def isNameAvailable(search, into):
+    for i in into:
+        if i['name'] == search:
+            return i['available'], i
+    return False, []
+
 # Loading and login accounts
 users = []
-print("[+] Loading users :")
+print("[+] Loading users..")
 for user in dbAccounts.data()['users']:
     handle = ThirdWorldWar(user['nickname'], user['password'], user['server'])
     if not handle.rLogin():
@@ -68,5 +82,36 @@ for user in dbAccounts.data()['users']:
 
 # Startup life loop
 while len(users) > 0:
-    print("[+] Working :")
+    print("[+] Loop life !")
+    for user in users:
+        print("[I] User \""+user['user'].account['email']+"\" was selected")
+        feed = user['user'].getFeeds()
+        if len(feed) == 0: continue
+        user['builds'] = user['user'].getBuildings()
+        user['techs'] = user['user'].getTechnology()
+        step = selectStep([user['builds'], user['techs']])
+        actions = selectActions(step)
+        if actions == None:
+            print("[I] User is level ("+str(step)+") max")
+            continue
+        else :
+            print("[I] User is level ("+str(step)+")")
+        i = getIndexFor('Buildings', feed)
+        if int(feed[i]['actives']) < int(feed[i]['max']):
+            n = int(feed[i]['actives'])
+            for action in actions:
+                b, data = isNameAvailable(action['entity'], user['builds'])
+                if b and int(data['level']) < int(action['level']):
+                    r = user['user'].rStartBuilding(action['entity'])
+                    if r:
+                        print("[Y] Starting \""+action['entity']+"\"")
+                        n = n +1
+                    else:
+                        print("[N] Starting \""+action['entity']+"\"")
+                    if n == int(feed[i]['max']):
+                        break
+        else :
+            print("[-] Building feed is full !")
+
+
     time.sleep(60)
